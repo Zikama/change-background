6d847822ee5ab1d40847381294001b856f59cdbf
Nehemie Zikama <Nehemiezikama@gmail.com>
2018-07-14 14:16:57 +0300

    added style and link to profile

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById("btn");
+    btn.addEventListener("click",()=>{
+        let change= document.getElementById("change");
+            change.classList.toggle("change");
+            change.classList.toggle("c");
+    });
