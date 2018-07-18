c9a299374a8ac3fbb5e148d062cfbf445fa91ffc
Nehemie Zikama <Nehemiezikama@gmail.com>
2018-07-18 06:33:51 +0300

    Modified 3 main files

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 let btn = document.getElementById("btn");
     btn.addEventListener("click",()=>{
-        let change= document.getElementById("change");
+        let change= document.getElementById("change"),second =document.getElementById("second");
             change.classList.toggle("change");
             change.classList.toggle("c");
-    });
+            second.classList.toggle("second");
+            second.classList.toggle("c"); 
+        });
